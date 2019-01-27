5b0035353f29ec2267afed6039a2898fcf7786ec
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2019-01-27 17:16:28 +0200

    Add files via upload

---
+++ tic-tac-toe/tic-tac-toe.ino
@@ -0,0 +1,29 @@
+int movePin = 6;
+int selectPin = 7;
+int cathodePin1 = 8;
+int cathodePin2 = 12;
+int cathodePin3 = 13;
+int anodePin1 = 11;
+int anodePin2 = 10;
+int anodePin3 = 9;
+void setup() 
+{
+  pinMode(movePin, INPUT_PULLUP);
+  pinMode(selectPin, INPUT_PULLUP);
+  pinMode(anodePin1, OUTPUT);
+  pinMode(anodePin2, OUTPUT);
+  pinMode(anodePin3, OUTPUT);
+  pinMode(cathodePin1, OUTPUT);
+  pinMode(cathodePin2, OUTPUT);
+  pinMode(cathodePin3, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(cathodePin1, LOW);
+  digitalWrite(cathodePin2, HIGH);
+  digitalWrite(cathodePin3, HIGH);
+  delay(50);
+  digitalWrite(anodePin1, HIGH);
+  delay(250);
+}
