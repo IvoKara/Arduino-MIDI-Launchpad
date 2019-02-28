0d14c31721fbfe16dc9ad70bd5d2e75d7eaed6bf
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2019-02-28 14:27:52 +0200

    Create piso.ino

---
+++ PISO register/piso.ino
@@ -0,0 +1,15 @@
+#define ShiftOut 8
+#define Clock 12
+#define Control 13
+
+void setup()
+{
+  pinMode(ShiftOut, INPUT);
+  pinMode(Clock, OUTPUT);
+  pinMode(Control, OUTPUT);
+}
+
+void loop()
+{
+  
+}
